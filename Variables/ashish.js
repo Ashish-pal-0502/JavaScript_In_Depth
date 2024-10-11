c66bb16555d9df66583a199e1a3487110b384fd6
Ashish Pal <ashishpal0502@gmail.com>
2024-10-11 04:41:13 +0000

    variable chapter done

--- Variables/ashish.js
+++ Variables/ashish.js
@@ -1,1 +1,11 @@
-console.log("this is ashish");
+const accountId = 12345
+let accounEmail = "ashish@google.com"
+var accountPassword = "12345"
+accountCity = "Raebareli"
+
+console.table([accounEmail, accountId, accountPassword, accountCity]);
+
+
+/*
+Do not use to prefer var now, bcz of the use of block scope and functional scope
+*/
